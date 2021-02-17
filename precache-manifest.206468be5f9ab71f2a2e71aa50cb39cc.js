905165e570b91c8d2091fbeb5072176e9b40fef9
misakumari <50099748+misakumari@users.noreply.github.com>
2021-02-17 17:37:51 +0530

    Updates

---
+++ precache-manifest.206468be5f9ab71f2a2e71aa50cb39cc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba78ad84191d34652a68e299deafdcea",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "53aa5ac2f34252eade89",
+    "url": "/ecommerce/static/css/main.9866fd19.chunk.css"
+  },
+  {
+    "revision": "5321f1ed931bae76561e",
+    "url": "/ecommerce/static/js/2.b965389e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ecommerce/static/js/2.b965389e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53aa5ac2f34252eade89",
+    "url": "/ecommerce/static/js/main.76c6c00c.chunk.js"
+  },
+  {
+    "revision": "7e919ce36a96358e560a",
+    "url": "/ecommerce/static/js/runtime-main.b674f9d9.js"
+  },
+  {
+    "revision": "5b27a2f385584fdc37653da53a3ec0a1",
+    "url": "/ecommerce/static/media/background1.5b27a2f3.jpg"
+  },
+  {
+    "revision": "e492df31a73e789ece99c4ac95324d15",
+    "url": "/ecommerce/static/media/crop.e492df31.jpg"
+  },
+  {
+    "revision": "eb7ab848f58af86b13ec32f0c7f25569",
+    "url": "/ecommerce/static/media/floral.eb7ab848.jpg"
+  },
+  {
+    "revision": "1c83f07080b135ff3c93c08e5f47fee9",
+    "url": "/ecommerce/static/media/onepiece.1c83f070.jpg"
+  }
+]);
